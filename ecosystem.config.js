76fd5823619b28a9b86371fd979dc5e3949f07a8
nam <ntn12800@gmail.com>
2022-04-26 13:35:27 +0700

    Edit ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "Excel handler",
+      exec_mode: "cluster",
+      instances: "1",
+      script: "./src/index.js", // your script
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: "3000",
+        TZ: "Asia/Ho_Chi_Minh",
+      },
+    },
+  ],
+};
